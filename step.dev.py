041b68f1aad05857ad668229f58be31232fdcc8e
Vladimir Khomyakov <rozendetr@bk.ru>
2023-10-17 08:40:49 +0000

    change tag parameters

--- step.dev.py
+++ step.dev.py
@@ -1,12 +1,17 @@
 from sinara.step import Step
 from sinara.step import StepSafeguard as sg
 
-try:
-    step = Step(step_params_file_globs="params/step_params.json",
-           env_name="user")
-    for notebook in step.notebooks:
-        notebook.run()
-except Exception as e:
-    step.handle_exception(e)
-finally:
-    step.handle_exit()
+step = Step(step_params_file_globs="params/step_params.json",
+        env_name="user")
+for notebook in step.notebooks:
+    notebook.run()
+
+# try:
+    # step = Step(step_params_file_globs="params/step_params.json",
+    #        env_name="user")
+    # for notebook in step.notebooks:
+    #     notebook.run()
+# except Exception as e:
+#     step.handle_exception(e)
+# finally:
+#     step.handle_exit()

401452c412cb863039e5cf5eba2f2beaca0c34cf
Vladimir Khomyakov <rozendetr@bk.ru>
2023-10-17 08:41:31 +0000

    rewrite step dev

--- step.dev.py
+++ step.dev.py
@@ -1,17 +1,12 @@
 from sinara.step import Step
 from sinara.step import StepSafeguard as sg
 
-step = Step(step_params_file_globs="params/step_params.json",
-        env_name="user")
-for notebook in step.notebooks:
-    notebook.run()
-
-# try:
-    # step = Step(step_params_file_globs="params/step_params.json",
-    #        env_name="user")
-    # for notebook in step.notebooks:
-    #     notebook.run()
-# except Exception as e:
-#     step.handle_exception(e)
-# finally:
-#     step.handle_exit()
+try:
+    step = Step(step_params_file_globs="params/step_params.json",
+           env_name="user")
+    for notebook in step.notebooks:
+        notebook.run()
+except Exception as e:
+    step.handle_exception(e)
+finally:
+    step.handle_exit()
